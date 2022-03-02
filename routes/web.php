a12c0585f15239b9adffb96dfcbd16a8b7475b60
Thomricco <tommasoricco@hotmail.it>
2022-03-02 15:56:12 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/link', function () {
+
+    $data = [
+        'link' => 'www.mondialxoo.it'
+    ];
+    return view('link', $data);
+});
